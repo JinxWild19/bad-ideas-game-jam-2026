b3ead3ea66978ba0da35374eec5d4b0c16b3ece4
JinxWIld19 <hunterarchieldcupit@gmail.com>
2026-03-01 23:00:54 -0600

    Made some update to the code with functions that should be able to be used by either the enmy of the player.

--- assets/components/Stats.gd
+++ assets/components/Stats.gd
@@ -2,8 +2,20 @@ extends Node
 
 class_name Stats
 
-@export var health: int
-@export var damage: int
-@export var damage_modifier: int
+@export var attack: int
+@export var attack_modifier: int
+@export var defence: int
+@export var defence_modifire: int
 @export var heal: int
 @export var heal_modifier: int
+@export var health: int
+@export var max_health: int
+
+func regain_health():
+	heal + heal_modifier == health
+
+func attack_amount():
+	attack + attack_modifier == attack
+
+func defence_amount():
+	attack - (defence + defence_modifire) == health
