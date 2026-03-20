ed0d17ca807103f0ed0934ea7a78ee06cb60cf91
JinxWild19 <hunterarchieldcupit@gmail.com>
2026-03-19 21:15:59 -0500

    Still trying to get the memories working with the watering.

--- assets/Memories/Flowers/flowers.gd
+++ assets/Memories/Flowers/flowers.gd
@@ -2,11 +2,14 @@ extends Node2D
 class_name Flowers
 
 @export_category("Watering Abbilities")
-@export var watering: Memory_Flowers
+@export var watering = get_parent().get_parent().get_node("Memory")
+
 func _ready():
 	watering.watering.connect(grow)
 	watering.stopped_watering.connect(grow)
 
 func grow():
 	if await watering._on_water_button_button_down() == true:
-		pass
+		print("Watering")
+	elif await watering._on_water_button_button_up() == true:
+		print("Stopped watering")
